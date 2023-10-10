e5412928cccbda157c6ba6bbfdee790ae59a538f
Verbiturum <user@example.com>
2023-10-11 00:30:43 +0300

    Add configs and tech stack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+	entry: "./src/script.js",
+	output: {
+		filename: "script.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(?:js|mjs|cjs)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							['@babel/preset-env', { targets: "defaults" }]
+						]
+					}
+				}
+			},
+		],
+	},
+	mode: "development",
+};
