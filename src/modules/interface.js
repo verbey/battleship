48b7e13813615d9bd3d67546478eb41871d9b776
Verbiturum <user@example.com>
2023-10-25 23:18:04 +0300

    Add Interface class constructor

---
+++ src/modules/interface.js
@@ -0,0 +1,7 @@
+class Interface {
+	constructor() {
+		this.left = document.querySelector(".left");
+		this.right = document.querySelector(".right");
+		this.generalMenu = document.querySelector(".generalMenu");
+	}
+}
