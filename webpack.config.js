ee45fdd0c94d263c2b0ab618cbdb129551f8d0c6
Verbiturum <user@example.com>
2023-11-08 04:04:03 +0200

    Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./src/script.js",
+	entry: "./src/main.js",
 	output: {
 		filename: "script.js",
 		path: path.resolve(__dirname, "dist"),
