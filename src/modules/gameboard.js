357db7eba12efc22657d010335f87d9dcfc1e59f
Verbiturum <user@example.com>
2023-12-23 15:15:39 +0200

    Remove unnecessary board fill

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -2,24 +2,18 @@ import Ship from "./ship";
 
 export default class Gameboard {
 	constructor(x, y) {
-		this.board = [];
-		for (let i = 0; i < x; i++) {
-			this.board.push([]);
-			for (let j = 0; j < y; j++) {
-				this.board[i].push(0);
-			}
-		}
-
+		this.xLimit = x;
+		this.yLimit = y;
 		this.ships = [];
 		this.targetedTiles = [];
 	}
 
 	addShip(size, direction, position) {
-		if (position[0] < 0 || position[0] > this.board.length || position[1] < 0 || position[1] > this.board.length) throw new Error("Invalid position coordinates.");
+		if (position[0] < 0 || position[0] > this.yLimit || position[1] < 0 || position[1] > this.xLimit) throw new Error("Invalid position coordinates.");
 
 		if (direction === "W" && position[0] - size < 0) throw new Error("Can't create a ship facing west here.");
-		else if (direction === "E" && position[0] + size > this.board.length) throw new Error("Can't create a ship facing east here.");
-		else if (direction === "N" && position[1] + size > this.board.length) throw new Error("Can't create a ship facing north here.");
+		else if (direction === "E" && position[0] + size > this.yLimit) throw new Error("Can't create a ship facing east here.");
+		else if (direction === "N" && position[1] + size > this.xLimit) throw new Error("Can't create a ship facing north here.");
 		else if (direction === "S" && position[1] - size < 0) throw new Error("Can't create a ship facing south here.");
 
 		const ship = new Ship(size, direction, position);
