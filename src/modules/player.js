8f0abfc39091518bba4710a2f5abcb6db9c096b3
Verbiturum <user@example.com>
2023-12-24 16:18:06 +0200

    Implement generateHitTarget method for Player class

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,22 +1,24 @@
+import Gameboard from "./gameboard";
+
 export default class PLayer {
 	constructor(name, type) {
 		this.name = name;
 		this.type = type;
-		this.hits = [];
+		this.gameboard = new Gameboard(10, 10);
 	}
 
-	addHit(limitX, limitY) {
-		if (limitX * limitY === this.hits.length) return undefined;
-
-		let x;
-		let y;
-		do {
-			x = Math.floor(Math.random() * (limitX));
-			y = Math.floor(Math.random() * (limitY));
+	generateHitTarget(gameboard) {
+		let allTiles = [];
+		for (let i = 0; i < gameboard.yLimit; i++) {
+			for (let j = 0; j < gameboard.xLimit; j++) allTiles.push([i, j]);
 		}
-		while (this.hits.find(hit => hit[0] === x && hit[1] === y) !== undefined);
-		let hit = [x, y];
-		this.hits.push(hit);
-		return hit;
+
+		const freeTiles = allTiles.filter(tile => {
+			const hitTile = gameboard.targetedTiles.find(hitTile => hitTile[0] === tile[0] && hitTile[1] === tile[1]);
+			return !hitTile;
+		});
+
+		const randomFreeTile = freeTiles[Math.floor(Math.random() * freeTiles.length)];
+		return randomFreeTile;
 	}
 }
