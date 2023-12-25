55d4ce2a9cfdf608d5585a203479941751dd663d
Verbiturum <user@example.com>
2023-12-25 15:59:58 +0200

    Rewrite the logic for placing ships

--- src/modules/interface.js
+++ src/modules/interface.js
@@ -1,29 +1,85 @@
 import Gameboard from "./gameboard";
+
 import Player from "./player";
 
 export default class Interface {
+
 	constructor() {
 		this.left = document.querySelector(".left");
 		this.right = document.querySelector(".right");
-		this.generalMenu = document.querySelector(".generalMenu");
+
 		this.selectedShipDirection = "E";
 	}
 
 	createPlayers() {
-		this.playerGameboard = new Gameboard(10, 10);
 		const playerNameInput = document.createElement("input");
 		playerNameInput.type = "text";
 		playerNameInput.placeholder = "Enter your name";
 		playerNameInput.classList.add("playerNameInput");
 		this.right.appendChild(playerNameInput);
 
+		this.createPlayerGameboard();
+
+		this.playerGameboard = new Gameboard(10, 10);
+
+		const finishPlayerCreation = document.createElement("button");
+		finishPlayerCreation.addEventListener("click", (event) => {
+
+			this.createPlayerGameboard();
+
+			this.player = new Player(playerNameInput.value);
+
+			this.opponentGameboard = new Gameboard(10, 10);
+
+			const startGame = document.createElement("button");
+			startGame.addEventListener("click", () => {
+				this.startGame();
+			});
+
+			startGame.textContent = "Start Game";
+			this.right.appendChild(startGame);
+
+			event.target.remove();
+
+		});
+
+		finishPlayerCreation.textContent = "Done";
+		this.right.appendChild(finishPlayerCreation);
+
+	}
+
+	createPlayerGameboard() {
+		// Clear old elements if they exist
+		while (this.left.firstChild) {
+			this.left.firstChild.remove();
+		}
+
+		const oldShipSelectionContainer = document.querySelector(".shipSelectionContainer");
+		if (oldShipSelectionContainer) oldShipSelectionContainer.remove();
+
+		for (let i = 0; i < 10; i++) {
+			for (let j = 0; j < 10; j++) {
+				const tile = document.createElement("div");
+				tile.classList.add("tile");
+				this.left.appendChild(tile);
+
+				tile.addEventListener("dragover", (e) => {
+					e.preventDefault();
+				});
+
+				tile.addEventListener("drop", (e) => {
+					const data = e.dataTransfer.getData("text");
+					e.target.appendChild(document.querySelector(`.${data}`));
+				});
+			}
+		}
+
 		const shipSelectionContainer = document.createElement("div");
 		shipSelectionContainer.classList.add("shipSelectionContainer");
 		this.right.appendChild(shipSelectionContainer);
 
 		const shipFourSize = document.createElement("div");
-		shipFourSize.classList.add("four");
-		shipFourSize.classList.add("0");
+		shipFourSize.classList.add("four-0");
 		shipFourSize.draggable = true;
 		shipSelectionContainer.appendChild(shipFourSize);
 
@@ -33,10 +89,10 @@ export default class Interface {
 
 		for (let i = 0; i < 2; i++) {
 			const shipThreeSize = document.createElement("div");
-			shipThreeSize.classList.add("three");
-			shipThreeSize.classList.add(`${i}`);
+			shipThreeSize.classList.add(`three-${i}`);
 			shipThreeSize.draggable = true;
 			shipSelectionContainer.appendChild(shipThreeSize);
+
 			shipThreeSize.addEventListener("dragstart", (e) => {
 				e.dataTransfer.setData("text", e.target.classList);
 			});
@@ -44,79 +100,26 @@ export default class Interface {
 
 		for (let i = 0; i < 3; i++) {
 			const shipTwoSize = document.createElement("div");
-			shipTwoSize.classList.add("two");
-			shipTwoSize.classList.add(`${i}`);
+			shipTwoSize.classList.add(`two-${i}`);
 			shipTwoSize.draggable = true;
 			shipSelectionContainer.appendChild(shipTwoSize);
+
 			shipTwoSize.addEventListener("dragstart", (e) => {
 				e.dataTransfer.setData("text", e.target.classList);
 			});
-
 		}
 
 		for (let i = 0; i < 4; i++) {
 			const shipOneSize = document.createElement("div");
-			shipOneSize.classList.add("one");
-			shipOneSize.classList.add(`${i}`);
+			shipOneSize.classList.add(`one-${i}`);
 			shipOneSize.draggable = true;
 			shipSelectionContainer.appendChild(shipOneSize);
+
 			shipOneSize.addEventListener("dragstart", (e) => {
 				e.dataTransfer.setData("text", e.target.classList);
 			});
 		}
 
-		for (let i = 0; i < this.playerGameboard.yLimit; i++) {
-			for (let j = 0; j < this.playerGameboard.xLimit; j++) {
-				const tile = document.createElement("div");
-				tile.classList.add("tile");
-				this.left.appendChild(tile);
-				tile.addEventListener("dragover", (e) => {
-					e.preventDefault();
-				});
-
-				tile.addEventListener("drop", (e) => {
-					const sizeString = e.dataTransfer.getData("text").split(" ")[0];
-					let size = 0;
-					if (sizeString === "one") size = 1;
-					else if (sizeString === "two") size = 2;
-					else if (sizeString === "three") size = 3;
-					else if (sizeString === "four") size = 4;
-					this.playerGameboard.addShip(size, this.selectedShipDirection, [i, j]);
-				});
-			}
-		}
-
-		const finishPlayerCreation = document.createElement("button");
-		finishPlayerCreation.addEventListener("click", (event) => {
-			this.player = new Player(playerNameInput.value);
-
-			this.opponentGameboard = new Gameboard(10, 10);
-
-			playerNameInput.value = "";
-
-			while (this.left.lastElementChild) {
-				this.left.removeChild(this.left.lastElementChild);
-			}
-
-
-
-			for (let i = 0; i < this.opponentGameboard.yLimit; i++) {
-				for (let j = 0; j < this.opponentGameboard.xLimit; j++) {
-					const tile = document.createElement("div");
-					tile.classList.add("tile");
-					this.left.appendChild(tile);
-				}
-			}
-
-			const startGame = document.createElement("button");
-			startGame.addEventListener("click", () => {
-				this.startGame();
-			});
-			startGame.textContent = "Start Game";
-			this.right.appendChild(startGame);
-			event.target.remove();
-		});
-		finishPlayerCreation.textContent = "Done";
-		this.right.appendChild(finishPlayerCreation);
 	}
+
 }
